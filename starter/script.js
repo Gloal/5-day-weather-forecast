f6c0bd902e5ba8c0f4123e920f39cab21111e351
Gloria A <alori.glo@gmail.com>
2023-12-16 02:09:57 +0000

    Sucessful api calls complete

--- starter/script.js
+++ starter/script.js
@@ -1,35 +1,56 @@
 $(document).ready(function () {
-    const APIKey = "9b9930a7106df81badb54c5976f6f846";    
+  const APIKey = "9b9930a7106df81badb54c5976f6f846";
+
+  $("#search-button-input").on("click", function (event) {
+    event.preventDefault();
+    console.log( event);
+
+   // const cityName = $("#search-input").val();
+    const cityName = "london";
+    console.log("CityName: " +cityName);
     
-    $("#search-form").on('submit', function (event) {
+    const queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${APIKey}`;
+    let secondQueryURL;
 
-        event.preventDefault();
+    fetch(queryURL)
+      .then (function (response) {
+        console.log("response1");
+        console.log(response);
+        return response.json();
+      })
+      .then (function (data){
+        console.log("data1");
+        console.log(data[0]);
+        console.log("returning geo data");
+        console.log("data.lat " + data[0].lat);
+        console.log("data.lon " + data[0].lon);
+        secondQueryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=${APIKey}`
+        return fetch(secondQueryURL)
+      })
+      .then (function (response){
+        console.log("response2");
+        console.log(response);
+        return response.json();
+      })
+      .then(function (data){
+        console.log("data2");
+        console.log(data);
+        console.log("city:" +data.city.name  )      
+        console.log("desciption:" +data.list[0].weather[0].description )      
+        const htmlContent = `<div class="weather-details">
+        <h2 class="city">${data.city.name}</h2>
+        <p class="date">Today's Date</p>
+        <p class="description">${data.list[0].weather[0].description}</p>
+        <p class="temp">Temperature: ${data.list[0].main.temp} </p>
+        <p class="wind">Wind: ${data.list[0].weather[0].description}</p>
+        <p class="humidity">Humidity:  ${data.list[0].main.humidity} </p>
+      </div>`
+      $(".js-weather-display-card").empty();
+      $(".js-weather-display-card").append(htmlContent);
+    })
+      .catch(function (error) {
+        console.error("Error fetching data:", error);
+      });
 
-        //TODO: endYear and startYear are optional - how to pass that onto the url in an optional way
-        //without having to give it a value
-
-        const searchCity = $(".search-button").val();
-        console.log(searchCity);
-
-        const recordsPerPage = $("#records-per-page").val();       
-
-        const queryURL = `https://api.openweathermap.org/data/2.5/forecast? lat={lat} &lon={lon} &appid=${APIkey}`;        
-        
-        fetch(queryURL)
-            .then(function (response) {
-
-                //TODO:why do we turn response to json when we already receive a json object from API?
-                return response.json();
-            })
-            .then(function (data) {
-                console.log(data);
-                $("#news-goes-here").empty();
-                data.response.docs.forEach(function (article) {
-                    $("#news-goes-here").append(`<div><a href=${article.web_url}>${article.headline.main}</a></div>`);
-                });
-            }) //TODO:Do we always need a catch? We were getting error message that wasnt printing //ALWAYS CATCH PROMISES!
-            .catch(function (error) {
-                console.error('Error fetching data:', error);
-            });
     });
 });
